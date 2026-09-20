0fa756b1211a2d01f409c3906c11d99d011680a2
Maulanahb <maulanahmad2287@gmail.com>
2026-09-20 12:24:02 +0700

    feat: tambah hapus item keranjang

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -14,6 +14,10 @@
             this.cart.push({ id, name, price });
         },
 
+        removeFromCart(id) {
+            this.cart = this.cart.filter(item => item.id !== id);
+        },
+
         subtotal() {
             return this.cart.reduce((sum, item) => sum + item.price, 0);
         }
@@ -43,7 +47,17 @@
     <div class="mt-4 border-t pt-3">
 
         <template x-for="item in cart" :key="item.id">
-            <p x-text="item.name + ' - Rp ' + item.price"></p>
+            <div class="flex items-center justify-between mb-2">
+                <p x-text="item.name + ' - Rp ' + item.price"></p>
+
+                <button
+                    type="button"
+                    @click="removeFromCart(item.id)"
+                    class="text-sm text-red-600 hover:text-red-800"
+                >
+                    Hapus
+                </button>
+            </div>
         </template>
 
         <p class="font-semibold mt-2">
